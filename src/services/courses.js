370c3eded561f27d59bc59722286d2026f2d5399
Ebelechukwu Doris Okolo <dorisebele47@gmail.com>
2024-09-24 16:30:40 +0100

    serving my json file with vercel

--- src/services/courses.js
+++ src/services/courses.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import store from "@/store";
 
+
+const BASE_URL = '/api/courses'; 
+
 export const getAllCourses = async () => {
-    const url = `http://localhost:3000/courses`; 
+   
+    // const url = `http://localhost:3000/courses`; 
     try {
-        const response = await axios.get(url); 
-        return response.data; 
+        const response = await axios.get(BASE_URL); 
+        return response.data.courses; 
     } catch (error) {
         console.error("Error fetching all courses:", error); 
         throw error; 
@@ -15,7 +19,7 @@ export const getCourseById = (id) => {
     // Get the course from the Vuex store
     const course = store.getters.getCourseById(id);
     if (course) {
-        return Promise.resolve(course); // Return a resolved promise with the course data
+        return Promise.resolve(course); 
     } else {
         return Promise.reject(new Error(`Course with ID ${id} not found in store`));
     }
